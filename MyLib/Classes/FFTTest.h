5f3887439f8980c70e57d731e4e11fc6d8ab870b
zuoyoupeng@wanmagroup.com <zuoyoupeng000>
2020-07-22 15:44:53 +0800

    初始化MyLib 库
    Signed-off-by: zuoyoupeng@wanmagroup.com <zuoyoupeng000>

---
+++ MyLib/Classes/FFTTest.h
@@ -0,0 +1,18 @@
+//
+//  FFTTest.h
+//  FFTUtil_Example
+//
+//  Created by icharge on 2020/7/22.
+//  Copyright © 2020 pengyouyoupeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FFTTest : NSObject
+@property (nonatomic,copy)NSString *name;
+- (void)run;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyLib/Classes/FFTTest.m
@@ -0,0 +1,15 @@
+//
+//  FFTTest.m
+//  FFTUtil_Example
+//
+//  Created by icharge on 2020/7/22.
+//  Copyright © 2020 pengyouyoupeng. All rights reserved.
+//
+
+#import "FFTTest.h"
+
+@implementation FFTTest
+- (void)run{
+    NSLog(@"zoule1");
+}
+@end
